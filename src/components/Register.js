46bc84eba1c2e54019055dd0cbf70d666d3c7e0a
Sangeeth Sudhakaran <Sangeeth.Sudhakaran@marlabs.com>
2023-03-06 10:58:19 +0530

    first commit

---
+++ src/components/Register.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { useState } from 'react';
+import axios from 'axios';
+
+import {
+    MDBBtn,
+    MDBContainer,
+    MDBCard,
+    MDBCardBody,
+    MDBInput,
+    MDBCheckbox
+}
+    from 'mdb-react-ui-kit';
+import './Register.css';
+
+function Register() {
+    const [fnameData, SetfnameData] = useState([]);
+    const [lnameData, SetlnameData] = useState([]);
+
+    function fnameOnchange(e) {
+        SetfnameData(e.target.value);
+    }
+
+    function lnameOnchange(e) {
+        SetlnameData(e.target.value);
+    }
+
+
+    function RegiserClickEvent() {
+        const data = { firstName: fnameData, lastName: lnameData, emailAddress: 'sangeeth@marlabs.com', password: '123' };
+
+        axios.post('https://localhost:44307/User/Register', data)
+            .then
+            .catch(error => {
+                this.setState({ errorMessage: error.message });
+                console.error('There was an error!', error);
+            });
+    }
+
+    return (
+        <MDBContainer fluid className='d-flex align-items-center justify-content-center bg-image' style={{ backgroundImage: 'url(https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp)' }}>
+            <div className='mask gradient-custom-3'></div>
+            <MDBCard className='m-5' style={{ maxWidth: '600px' }}>
+                <MDBCardBody className='px-5'>
+                    <h2 className="text-uppercase text-center mb-5">Create an account</h2>
+                    <MDBInput onChange={fnameOnchange} wrapperClass='mb-4' label='Your Name' size='lg' id='form1' type='text' />
+                    <MDBInput onChange={lnameOnchange} wrapperClass='mb-4' label='Last Name' size='lg' id='form55' type='text' />
+                    <MDBInput wrapperClass='mb-4' label='Your Email' size='lg' id='form2' type='email' />
+                    <MDBInput wrapperClass='mb-4' label='Password' size='lg' id='form3' type='password' />
+                    <MDBInput wrapperClass='mb-4' label='Repeat your password' size='lg' id='form4' type='password' />
+                    <div className='d-flex flex-row justify-content-center mb-4'>
+                        <MDBCheckbox name='flexCheck' id='flexCheckDefault' label='I agree all statements in Terms of service' />
+                    </div>
+                    <MDBBtn onClick={RegiserClickEvent} className='mb-4 w-100 gradient-custom-4' size='lg'>Register</MDBBtn>
+                </MDBCardBody>
+            </MDBCard>
+        </MDBContainer>
+    );
+
+
+
+}
+
+export default Register;
---
+++ src/components/UsersList.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import axios from "axios";
+
+const baseURL = "https://localhost:44307/User/GetAll";
+export default function UsersList() {
+    const [post, setPost] = React.useState(null);
+
+    React.useEffect(() => {
+        axios.get(baseURL).then((response) => {
+            setPost(response.data);
+            console.log(response.data);
+        });
+    }, []);
+
+    if (!post) return null;
+
+    return (
+        <div>{post.map((p) => {
+            const list = (
+              <>
+                <ul>
+                  <li>Id: {p.id}</li>
+                  <li>Name: {p.firstName} {p.lastName}</li>
+                  <li>emailAddress: {p.emailAddress}</li> 
+                </ul>
+                <hr />
+              </>
+            );
+            return list;
+          })}
+        </div>
+    );
+}
